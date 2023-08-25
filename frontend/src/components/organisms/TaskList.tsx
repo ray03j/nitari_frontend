29b17c6220661213cbe481b79d854843cc8849fe
ray03j <eihouse.potato@outlook.jp>
2023-08-26 00:44:33 +0900

    [add] TaskList.tsx

---
+++ frontend/src/components/organisms/TaskList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { Card, CardProps } from "../molecules/Card";
+
+type TaskListProps = {
+  Tasks: CardProps[];
+};
+
+export const TaskList: React.FC<TaskListProps> = ({Tasks}) => {
+
+
+  return (
+    <>
+    {Tasks.map((task, index) => (
+      <div key={index}>
+        <Card
+          taskName=""
+          taskDescription=""
+          startDate=""
+          dueDate=""
+          />
+      </div>
+    ))}
+    </>
+  );
+};
